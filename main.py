e5b997b8eeedee60b3e8577cf6eb1841ed1d49e5
Ian122179 <76274387+Ian122179@users.noreply.github.com>
2021-02-01 11:25:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import time
 import os
-def clear():
-    os.system('cls')
-def repeat():
+
+while True:
     time1 = time.strftime('%I:%M:%p')
     print(time1)
 
@@ -11,37 +10,32 @@ def repeat():
         os.startfile("school.mp3")
         time.sleep(5)
         os.startfile("wakeup.wav")
-        time.sleep(60)
+        time.sleep(10)
 
     if time1 == '08:00:AM':
         print("me works")
         os.startfile("scool.mp3")
         time.sleep(5)
         os.startfile("engineering.wav")
-        time.sleep(60)
+        time.sleep(10)
 
     if time1 == '10:00:AM':
         print("me works")
         os.startfile("school.mp3")
         time.sleep(5)
         os.startfile("math.wav")
-        time.sleep(60)
+        time.sleep(10)
 
     if time1 == '11:00:AM':
         print("me works")
         os.startfile("school.mp3")
         time.sleep(5)
         os.startfile("other.wav")
-        time.sleep(60)
+        time.sleep(10)
 
     if time1 == '11:30:AM':
         print("me works")
         os.startfile("school.mp3")
         time.sleep(5)
         os.startfile("schoolout.wav")
-        time.sleep(60)
-
-    clear()
-    time.sleep(5)
-    repeat()
-repeat()
+        time.sleep(10)
