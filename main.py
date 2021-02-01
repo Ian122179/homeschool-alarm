caae01f316b85ec40ce1fdf28096216f1d0e4f8c
Ian122179 <76274387+Ian122179@users.noreply.github.com>
2021-02-01 11:21:46 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import time
+import os
+def clear():
+    os.system('cls')
+def repeat():
+    time1 = time.strftime('%I:%M:%p')
+    print(time1)
+
+    if time1 == '07:30:AM':
+        print("me works")
+        os.startfile("school.mp3")
+        time.sleep(5)
+        os.startfile("wakeup.wav")
+        time.sleep(60)
+
+    if time1 == '08:00:AM':
+        print("me works")
+        os.startfile("scool.mp3")
+        time.sleep(5)
+        os.startfile("engineering.wav")
+        time.sleep(60)
+
+    if time1 == '10:00:AM':
+        print("me works")
+        os.startfile("school.mp3")
+        time.sleep(5)
+        os.startfile("math.wav")
+        time.sleep(60)
+
+    if time1 == '11:00:AM':
+        print("me works")
+        os.startfile("school.mp3")
+        time.sleep(5)
+        os.startfile("other.wav")
+        time.sleep(60)
+
+    if time1 == '11:30:AM':
+        print("me works")
+        os.startfile("school.mp3")
+        time.sleep(5)
+        os.startfile("schoolout.wav")
+        time.sleep(60)
+
+    clear()
+    time.sleep(5)
+    repeat()
+repeat()
