732abc4a6807b7f72788487cc7840e7d81596310
Ian122179 <76274387+Ian122179@users.noreply.github.com>
2021-02-03 13:11:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import time
-import os
+
+import pygame as pygame
+
+pygame.init()
+schoolbell = pygame.mixer.Sound("school.mp3")
+wakeup = pygame.mixer.Sound("wakeup.wav")
+engineering = pygame.mixer.Sound("engineering.wav")
+math = pygame.mixer.Sound("math.wav")
+other = pygame.mixer.Sound("other.wav")
+schoolout = pygame.mixer.Sound("schoolout.wav")
 
 while True:
     time1 = time.strftime('%I:%M:%p')
@@ -7,35 +16,37 @@ while True:
 
     if time1 == '07:30:AM':
         print("me works")
-        os.startfile("school.mp3")
+        pygame.mixer.Sound.play(schoolbell)
         time.sleep(5)
-        os.startfile("wakeup.wav")
-        time.sleep(10)
+        pygame.mixer.Sound.play(wakeup)
+        time.sleep(60)
 
     if time1 == '08:00:AM':
         print("me works")
-        os.startfile("scool.mp3")
+        pygame.mixer.Sound.play(schoolbell)
         time.sleep(5)
-        os.startfile("engineering.wav")
-        time.sleep(10)
+        pygame.mixer.Sound.play(engineering)
+        time.sleep(60)
 
     if time1 == '10:00:AM':
         print("me works")
-        os.startfile("school.mp3")
+        pygame.mixer.Sound.play(schoolbell)
         time.sleep(5)
-        os.startfile("math.wav")
-        time.sleep(10)
+        pygame.mixer.Sound.play(math)
+        time.sleep(60)
 
     if time1 == '11:00:AM':
         print("me works")
-        os.startfile("school.mp3")
+        pygame.mixer.Sound.play(schoolbell)
         time.sleep(5)
-        os.startfile("other.wav")
-        time.sleep(10)
+        pygame.mixer.Sound.play(other)
+        time.sleep(60)
 
-    if time1 == '11:30:AM':
+    if time1 == '11:30:PM':
         print("me works")
-        os.startfile("school.mp3")
+        pygame.mixer.Sound.play(schoolbell)
         time.sleep(5)
-        os.startfile("schoolout.wav")
-        time.sleep(10)
+        pygame.mixer.Sound.play(schoolout)
+        time.sleep(60)
+
+    time.sleep(5)
